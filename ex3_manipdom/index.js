7504b385e0011096752a3263010642336e0acc55
Alex <al.vaccaro@outlook.com>
2024-01-26 12:16:51 +0100

    svolto esercizio 3 manipolare il dom

--- ex3_manipdom/index.js
+++ ex3_manipdom/index.js
@@ -1,3 +1,24 @@
-const addProduct = () => {
-  // ...
-};
+function addProduct() {
+  var taskValue = document.querySelector('input').value;
+
+  //  x non aggiungere una checkbox vuota 
+  if (taskValue !== '') {
+
+    // creo li e checkbox
+    var li = document.createElement('li');
+
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    // Appendo testo  e checkbox a li.
+    li.appendChild(document.createTextNode(taskValue));
+    li.appendChild(checkbox);
+
+    // appendo li a ul
+    const ul= document.querySelector('ul')
+    ul.appendChild(li);
+
+    // Pulisco il campo di testo dopo l'aggiunta
+    document.querySelector('input').value = '';
+  }
+}
